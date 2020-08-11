c0dc6449d4f4b56a0a64004911bc5139ae407d4c
Hasan Qasim <hasanqasim92@gmail.com>
2020-08-11 20:24:16 +1000

    implement DataSource & TableView protocol on CategoryVC

--- rashie-store/Controller/CategoriesVC.swift
+++ rashie-store/Controller/CategoriesVC.swift
@@ -16,13 +16,24 @@ class CategoriesVC: UIViewController, UITableViewDataSource, UITableViewDelegate
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        categoryTable.delegate = self
+        categoryTable.dataSource = self
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return DataService.instance.getCategories().count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        <#code#>
+        if let cell = tableView.dequeueReusableCell(withIdentifier: "categoryCell") as? CategoryCell {
+            let category = DataService.instance.getCategories()[indexPath.row]
+            //cell.categoryImage.image = UIImage(named: category.imageName)
+            //cell.categoryTitle.text = category.title
+            cell.updateViews(category: category)
+            return cell
+        } else {
+            return CategoryCell()
+        }
     }
 
 
