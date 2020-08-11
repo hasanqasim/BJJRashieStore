eec885c56fb7bd623d4f234848ad93a121cc0df6
Hasan Qasim <hasanqasim92@gmail.com>
2020-08-11 20:59:01 +1000

    add CollectionView & custom ColectionViewCell class

---
+++ rashie-store/View/ProductCell.swift
@@ -0,0 +1,15 @@
+//
+//  ProductCell.swift
+//  rashie-store
+//
+//  Created by Hasan Qasim on 11/8/20.
+//  Copyright © 2020 Hasan Qasim. All rights reserved.
+//
+
+import UIKit
+
+class ProductCell: UICollectionViewCell {
+    @IBOutlet weak var productImage: UIImageView!
+    @IBOutlet weak var productTitle: UILabel!
+    @IBOutlet weak var productPrice: UILabel!
+}
